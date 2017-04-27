48f454bf003551b4dc9a00793c6db93c35a19afb
Ryan Larkin <ryan.r.larkin@gmail.com>
2017-04-27 19:32:25 +0000

    Day 2 almost final

--- js/app-two.js
+++ js/app-two.js
@@ -2,16 +2,15 @@
     var FLICKR_URL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1';
     var FLICKR_KEY = '12598f859b86a14b2f925b8d0e422989';
     
-    function getPhotosForSearch(searchTerm) {
-        var url = `${FLICKR_URL}&api_key=${FLICKR_KEY}&text=${searchTerm}`;
+    function getPhotosForSearch(searchTerm,page) {
+        var url = `${FLICKR_URL}&api_key=${FLICKR_KEY}&text=${searchTerm}&page=${page}`;
         
         return fetch(url)
         .then(response => response.json())
         .then(pictures => {
             var picturesArray = [];
-            
+            console.log(pictures);
             var pictureData = pictures.photos.photo;
-            console.log(pictureData);
             var pictureUrl = '';
             
             pictureData.forEach(picture => {
@@ -23,7 +22,6 @@
                     title: picture.title
                 });
             });
-            console.log(picturesArray);
             return picturesArray;
         });
     }
@@ -38,20 +36,20 @@
         image.setAttribute('alt', photoData.title);
         
         link.appendChild(image);
-        
         return link;
     }
     
-    var imageForm = document.querySelector('.imageForm');
-    var searchInput = document.querySelector('.searchInput');
+    var imageForm = document.querySelector('.image-form');
+    var searchInput = document.querySelector('.search-input');
     var gallery = document.querySelector('.gallery');
     
     imageForm.addEventListener('submit', function() {
+        gallery.innerHTML = "";
         event.preventDefault();
         
         var searchTerm = searchInput.value;
         
-        getPhotosForSearch(searchTerm)
+        getPhotosForSearch(searchTerm, 1)
         .then(photos => {
             photos.forEach(photo => {
                 var imageLink = document.createElement('li');
@@ -61,4 +59,48 @@
             });
         });
     });
+    
+    var page = 1;
+    var ticking = false;
+    
+    document.addEventListener('scroll', function(event) {
+        ticking = false;
+        setTimeout(function(e) {
+            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !ticking) {    
+                page++;
+                var searchTerm = document.querySelector('.search-input').value;
+                console.log(searchTerm);
+                getPhotosForSearch(searchTerm, page)
+                .then(photos => {
+                    photos.forEach(photo => {
+                        var imageLink = document.createElement('li');
+                        var imageThumb = createFlickrThumb(photo);
+                        imageLink.appendChild(imageThumb);
+                        gallery.appendChild(imageLink);
+                    });
+                });
+                ticking = false;
+            }
+            ticking = true;
+        }, 500);
+            
+    });
 })();
+
+var last_known_scroll_position = 0;
+var ticking = false;
+
+function doSomething(scroll_pos) {
+  // do something with the scroll position
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+  }
+  ticking = true;
+});
--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@
     
     var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
     var GOOGLE_MAPS_API_KEY = 'AIzaSyBdpDOpjoue2bvABv9xSdYY6SDdwvtKM30';
+    var FORMATTED_ADDRESS = '';
+    
+    
     
     // This function returns a promise that will resolve with an object of lat/lng coordinates
     function getCoordinatesForCity(cityName) {
@@ -14,7 +17,10 @@
         return (
             fetch(url) // Returns a promise for a Response
             .then(response => response.json()) // Returns a promise for the parsed JSON
-            .then(data => data.results[0].geometry.location) // Transform the response to only take what we need
+            .then(data => {
+                FORMATTED_ADDRESS = data.results[0]['formatted_address'];
+                return data.results[0].geometry.location;
+            }) // Transform the response to only take what we need
         );
     }
     
@@ -33,6 +39,7 @@
     var cityForm = app.querySelector('.city-form');
     var cityInput = cityForm.querySelector('.city-input');
     
+    var locationAddress = app.querySelector('.location-address');
     var currentWeather = app.querySelector('.current-weather');
     var currentTemperature = app.querySelector('.current-temperature');
     
@@ -44,47 +51,47 @@
         getCoordinatesForCity(city) // get the coordinates for the input city
         .then(getCurrentWeather) // get the weather for those coordinates
         .then(function(weather) { // set the current temperature display
-        console.log(weather);
-            // switch(weather.icon) {
-            //     case 'clear-day':
-            //         document.body.style.backgroundImage = "url('http://www.quotemaster.org/images/b1/b149c31dd5976f75c17a63165fe96af9.jpg')";
-            //         break;
-            //     case 'clear-night':
-            //         document.body.style.backgroundImage = "url('')";
-            //         break;
-            //     case 'rain':
-            //         document.body.style.backgroundImage = "url('')";
-            //         break;
-            //     case 'snow':
-            //         document.body.style.backgroundImage = "url('')";
-            //         break;
-            //     case 'sleet':
-            //         document.body.style.backgroundImage = "url('')";
-            //         break;
-            //     case 'wind':
-            //         document.body.style.backgroundImage = "url('')";
-            //         break;
-            //     case 'fog':
-            //         document.body.style.backgroundImage = "url('')";
-            //         break;
-            //     case 'cloudy':
-            //         document.body.style.backgroundImage = "url('')";
-            //         break;
-            //     case 'partly-cloudy-day':
-            //         document.body.style.backgroundImage = "url('')";
-            //         break;
-            //     case 'partly-cloudy-night':
-            //         document.body.style.backgroundImage = "url('')";
-            //         break;
-            //     default:
-            //         document.body.style.backgroundImage = "url('http://everythingnonfiction.typepad.com/Stormy%20weather.JPG')";
-            // }
+            locationAddress.style.visibility = "visible";
+            locationAddress.innerHTML = 'Weather For: ' + FORMATTED_ADDRESS;
             
-            currentWeather.style.visibility = "visible"
+            currentWeather.style.visibility = "visible";
             currentWeather.innerHTML = 'Current Weather: ' + weather.summary + '&nbsp;';
             
-            currentTemperature.style.visibility = "visible"
+            currentTemperature.style.visibility = "visible";
             currentTemperature.innerHTML = 'Current Temperature: ' + weather.temperature + ' &deg;C ';
+            console.log(weather.icon);
+            switch(weather.icon) {
+                case 'clear-day':
+                    document.body.style.backgroundImage = "url('images/clear.jpg')";
+                    break;
+                case 'clear-night':
+                    document.body.style.backgroundImage = "url('images/clear-night.jpg')";
+                    break;
+                case 'rain':
+                    document.body.style.backgroundImage = "url('images/rain.jpg')";
+                    break;
+                case 'snow':
+                    document.body.style.backgroundImage = "url('images/snow.jpg')";
+                    break;
+                case 'sleet':
+                    document.body.style.backgroundImage = "url('images/sleet.jpg')";
+                    break;
+                case 'wind': 
+                case 'cloudy':
+                case 'partly-cloudy-day':
+                case 'partly-cloudy-night':
+                    document.body.style.backgroundImage = "url('images/cloudy.jpg')";
+                    break;
+                case 'fog':
+                    document.body.style.backgroundImage = "url('images/fog.jpg')";
+                    break;
+                default:
+                    document.body.style.backgroundImage = "url('images/default.jpg')";
+            }
         });
+    });
+    
+    google.maps.event.addDomListener(window, 'load', function () {
+        var places = new google.maps.places.Autocomplete(document.querySelector('.city-input'));
     });
 })();
